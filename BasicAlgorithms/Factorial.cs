0718ea0539a82c86205703799b97b9c057c7ccdf
Arokiadoss Bilavendran <ba.doss2000@gmail.com>
2018-10-14 11:13:53 -0500

    Basic algorithm project created. First commit for basic algorithm project
    Basic algorithm project created, factorial with recursion option coded.

---
+++ BasicAlgorithms/Factorial.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicAlgorithms
+{
+    public class Factorial
+    {
+        public ulong FactorialUsingRecursion(ulong factorial)
+        {
+            if (factorial <= 1) return 1; // the factorial value for 0 and 1 is 1
+            return factorial * FactorialUsingRecursion(factorial - 1);
+        }
+    }
+}
---
+++ BasicAlgorithms/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BasicAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Factorial using recursion testing
+            Console.WriteLine("Enter a number to calculate factorial: ");
+            ulong lngFactorial = ulong.Parse(Console.ReadLine());
+            Console.WriteLine($"The factorial value for {lngFactorial} is: {new Factorial().FactorialUsingRecursion(lngFactorial)}");
+            Console.ReadLine();
+        }
+    }
+}
